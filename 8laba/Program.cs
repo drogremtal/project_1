d14667b25fb022e976f94d606f432fb756d80898
Drogremtal <bp240@outlook.com>
2019-03-18 20:49:52 +0300

    Добавьте файлы проекта.

---
+++ 8laba/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace _8laba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textFromFile = string.Empty;
+            #region Считывание контейнера с файла
+            using (FileStream fstream = File.OpenRead(@"input.txt"))
+            {
+                // преобразуем строку в байты
+                byte[] array = new byte[fstream.Length];
+                // считываем данные
+                fstream.Read(array, 0, array.Length);
+                // декодируем байты в строку
+                textFromFile = System.Text.Encoding.UTF8.GetString(array);
+
+            }
+            #endregion
+
+            #region Удаляем с пробелы в конце и в начале строки
+            var s = textFromFile.Split('.');
+            string out_text = string.Empty;
+
+            var res = s;
+
+            int i = 0;
+            foreach (var item in s)
+            {
+                res[i] = item.TrimEnd(' ');
+                res[i] = res[i].TrimStart(' ');
+                i++;
+            }
+            #endregion
+
+            string text = "Hello";
+
+            string bintext = new Service().TextToBin(text);
+            string test = "10101010101110101101";
+
+            int k = test.Length < res.Length ? test.Length : res.Length;
+
+            for (int t = 0; t < res.Length; t++)
+            {
+                res[t] += ".";
+            }
+
+            for (int j = 0; j <k; j++)
+            {
+                if (string.Equals(test[j],'0'))
+                {
+                    res[j] += "  ";}
+                else
+                {
+                    res[j] += " ";
+                }
+            }
+
+            out_text = string.Join("",res);
+
+            using (FileStream fstream = new FileStream(@"output.txt", FileMode.OpenOrCreate))
+            {
+                // преобразуем строку в байты
+                byte[] array = System.Text.Encoding.UTF8.GetBytes(out_text);
+                // запись массива байтов в файл
+                fstream.Write(array, 0, array.Length);
+                Console.WriteLine("Текст записан в файл");
+            }
+
+
+            using (FileStream fstream = File.OpenRead(@"output.txt"))
+            {
+                // преобразуем строку в байты
+                byte[] array = new byte[fstream.Length];
+                // считываем данные
+                fstream.Read(array, 0, array.Length);
+                // декодируем байты в строку
+                textFromFile = System.Text.Encoding.UTF8.GetString(array);
+
+            }
+
+
+            var intext = textFromFile.Split('.');
+
+            string outtex = string.Empty;
+
+            foreach (var item in intext)
+            {
+                for (int t = 0; t < item.Length; t++)
+                {
+                    if (string.Equals(item[0],' ') && string.Equals(item[1],' '))
+                    {
+                        outtex += "0";break;
+                    }
+                    else
+                        if (string.Equals(item[0],' '))
+                    {
+                        outtex += "1";break;
+                    }
+                }
+            }
+
+
+
+
+
+            Console.WriteLine(out_text);
+
+
+            Console.WriteLine(outtex);
+            Console.WriteLine(test);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 8laba/Service.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8laba
+{
+    public class Service
+    {
+        public string TextToBin(string text)
+        {
+
+            string res_out = "";
+
+            int[] input_n = new int[text.Length];
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Convert.ToString(text[i], 2).Length == 6)
+                {
+                    res_out += "00" + Convert.ToString(text[i], 2);
+                }
+                else
+                if (Convert.ToString(text[i], 2).Length == 7)
+                {
+                    res_out += "0" + Convert.ToString(text[i], 2);
+                }
+                else
+                {
+                    res_out += Convert.ToString(text[i], 2);
+                }
+                input_n[i] = Convert.ToInt32(Convert.ToString(text[i], 2), 2);
+            }
+
+        return res_out;
+        }
+
+    }
+}
